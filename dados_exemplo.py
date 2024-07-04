f62d14a0b2ed9f6cf3b2f8612de35e78f0b94b63
pedrofcelidonio <pedrofcelidonio@gmail.com>
2024-07-04 18:32:33 -0300

    primeiro lancamento do arquivo de dados

---
+++ dados_exemplo.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import random
+
+tamanho=500
+
+individuos = {'ids':np.arange(1,tamanho+1,1),
+             'Q1':np.random.randint(1, 6, tamanho),
+             'Q2':np.random.randint(1, 6, tamanho),
+             'Q3':np.random.randint(1, 6, tamanho),
+             'Q4':np.random.randint(1, 6, tamanho),
+             'Q5':np.random.randint(1, 6, tamanho),
+             'Q6':np.random.randint(1, 6, tamanho),
+             'Q7':np.random.randint(1, 6, tamanho),
+             'Q8':np.random.randint(1, 6, tamanho),
+             'Q9':np.random.randint(1, 6, tamanho),
+             'fpd30':np.random.binomial(n=1, p=0.25, size=tamanho)}
+
+df = pd.DataFrame(individuos)
+df['ids']='n'+df.ids.astype(str)
