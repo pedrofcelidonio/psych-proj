e742b93b32b8e3b6d47facc85cfa2c99f156ebf4
pedrofcelidonio <pedrofcelidonio@gmail.com>
2024-07-04 22:02:12 -0300

    criacao novo arquivo para passo de merge final e variavel nova no arquivo inicial para acompanhamento de etapa de merge final

--- dados_exemplo_3.py
+++ dados_exemplo_3.py
@@ -5,7 +5,6 @@ import random
 tamanho=500
 tamanho2=1000
 variavel_teste='branch'
-variavel_teste='merge'
 
 individuos = {'ids':np.arange(1,tamanho+1,1),
              'Q1':np.random.randint(1, 6, tamanho),
